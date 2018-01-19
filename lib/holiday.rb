28685f48881e86b0c7a6f07a4cfba1677a3a971c
Melissa Sames <melissa.sames@gmail.com>
2018-01-19 04:55:48 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,14 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_supplies = []
 
+  holiday_hash.each { |season, holiday|
+    holiday.each { |supplies_array|
+      holiday_supplies << supplies_array
+    }
+  }
+  holiday_supplies.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
